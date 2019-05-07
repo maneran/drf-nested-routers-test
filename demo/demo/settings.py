1b23f135573edb2f504e037ca67bd7efcf992abc
Ran Manela <RANMA@amdocs.com>
2019-05-07 17:26:57 +0300

    Update settings.py

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -80,12 +80,6 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'imdb2',
-        'USER': 'postgres',
-        'PASSWORD': 'Unix_11!',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
     }
 }
 
